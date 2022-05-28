7e07c4ca413b6b1f68199a55ffe8b0360b8faf00
Wayne Brenckle <waynebrenckle@gmail.com>
2022-05-28 11:41:24 -0700

    remove .dep/

--- .dep/vectors.o.d
+++
@@ -1,8 +0,0 @@
-build/obj/vectors.o: \
- ../../../../Dev/ChibiOS_21.11.1/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
- ../../../../Dev/ChibiOS_21.11.1/os/common/startup/ARMCMx/devices/STM32L4xx/cmparams.h \
- ../../../../Dev/ChibiOS_21.11.1/os/hal/boards/ST_NUCLEO144_L496ZG/board.h
-
-../../../../Dev/ChibiOS_21.11.1/os/common/startup/ARMCMx/devices/STM32L4xx/cmparams.h:
-
-../../../../Dev/ChibiOS_21.11.1/os/hal/boards/ST_NUCLEO144_L496ZG/board.h:
